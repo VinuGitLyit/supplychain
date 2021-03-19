b681d26e4e33bb6ffd02b36577236d314af18de7
VinuGitLyit <80859377+VinuGitLyit@users.noreply.github.com>
2021-03-19 20:19:48 +0000

    Update App.java

--- src/main/java/lyit/ie/supplychain/App.java
+++ src/main/java/lyit/ie/supplychain/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Supply Chain App for ABC Stores!" );
-        System.out.println( "Lab 3 - Git hub in action !!!!")
+        System.out.println( "Lab 3 - Git hub in action !!!!");
     }
 }
