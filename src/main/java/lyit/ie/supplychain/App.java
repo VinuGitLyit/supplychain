5537c931e8f6b939ecd8bda1b3e68cabc9da4994
VinuGitLyit <L00162799@student.lyit.ie>
2021-03-19 19:54:44 +0000

    Initial file added

---
+++ src/main/java/lyit/ie/supplychain/App.java
@@ -0,0 +1,13 @@
+package lyit.ie.supplychain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
